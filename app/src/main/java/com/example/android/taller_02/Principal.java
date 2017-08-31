23a8bd8ca5047c6fb18e8195821b0eba82dc2203
“Alexhamb” <alex6_10@hotmail.com>
2017-08-30 23:53:29 -0500

    terminado taller

--- app/src/main/java/com/example/android/taller_02/Principal.java
+++ app/src/main/java/com/example/android/taller_02/Principal.java
@@ -79,26 +79,171 @@ public class Principal extends AppCompatActivity {
     }
 
     public void Comprar(View v){
+        int valorU=0, valorT, canti;
+        canti =Integer.parseInt ( cantidad.getText().toString());
         if(rPesos.isChecked()){
-
             if(comboMaterial.equals(getResources().getString(R.string.str_cuero))
-                && comboDije.equals(getResources().getString(R.string.str_martillo))
-                    && comboTipo.equals(getResources().getString(R.string.str_oro))
-                    )
-            {
-
+                    && comboDije.equals(getResources().getString(R.string.str_martillo))
+                    && comboTipo.equals(getResources().getString(R.string.str_oro))){
                 costo = 100;
-
             }
-
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuero))
+                    && comboDije.equals(getResources().getString(R.string.str_martillo))
+                    && comboTipo.equals(getResources().getString(R.string.str_rosado))){
+                costo = 100;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuero))
+                    && comboDije.equals(getResources().getString(R.string.str_martillo))
+                    && comboTipo.equals(getResources().getString(R.string.str_plata))){
+                costo = 80;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuero))
+                    && comboDije.equals(getResources().getString(R.string.str_martillo))
+                    && comboTipo.equals(getResources().getString(R.string.str_niquel))){
+                costo = 70;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuero))
+                    && comboDije.equals(getResources().getString(R.string.str_ancla))
+                    && comboTipo.equals(getResources().getString(R.string.str_oro))){
+                costo = 120;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuero))
+                    && comboDije.equals(getResources().getString(R.string.str_ancla))
+                    && comboTipo.equals(getResources().getString(R.string.str_rosado))){
+                costo = 120;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuero))
+                    && comboDije.equals(getResources().getString(R.string.str_ancla))
+                    && comboTipo.equals(getResources().getString(R.string.str_plata))){
+                costo = 100;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuero))
+                    && comboDije.equals(getResources().getString(R.string.str_ancla))
+                    && comboTipo.equals(getResources().getString(R.string.str_niquel))){
+                costo = 90;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuerda))
+                    && comboDije.equals(getResources().getString(R.string.str_martillo))
+                    && comboTipo.equals(getResources().getString(R.string.str_oro))){
+                costo = 90;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuerda))
+                    && comboDije.equals(getResources().getString(R.string.str_martillo))
+                    && comboTipo.equals(getResources().getString(R.string.str_rosado))){
+                costo = 120;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuerda))
+                    && comboDije.equals(getResources().getString(R.string.str_martillo))
+                    && comboTipo.equals(getResources().getString(R.string.str_plata))){
+                costo = 70;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuerda))
+                    && comboDije.equals(getResources().getString(R.string.str_martillo))
+                    && comboTipo.equals(getResources().getString(R.string.str_niquel))){
+                costo = 50;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuerda))
+                    && comboDije.equals(getResources().getString(R.string.str_ancla))
+                    && comboTipo.equals(getResources().getString(R.string.str_oro))){
+                costo = 110;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuerda))
+                    && comboDije.equals(getResources().getString(R.string.str_ancla))
+                    && comboTipo.equals(getResources().getString(R.string.str_rosado))){
+                costo = 110;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuerda))
+                    && comboDije.equals(getResources().getString(R.string.str_ancla))
+                    && comboTipo.equals(getResources().getString(R.string.str_plata))){
+                costo = 90;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuerda))
+                    && comboDije.equals(getResources().getString(R.string.str_ancla))
+                    && comboTipo.equals(getResources().getString(R.string.str_niquel))){
+                costo = 80;
+            }
         }
-
-        if(rdolares.isChecked()){
-
-            totalPagar.setText("Dolares");
-
+        if(rPesos.isChecked()){
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuero))
+                    && comboDije.equals(getResources().getString(R.string.str_martillo))
+                    && comboTipo.equals(getResources().getString(R.string.str_oro))){
+                costo = 100 * 3200;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuero))
+                    && comboDije.equals(getResources().getString(R.string.str_martillo))
+                    && comboTipo.equals(getResources().getString(R.string.str_rosado))){
+                costo = 100 * 3200;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuero))
+                    && comboDije.equals(getResources().getString(R.string.str_martillo))
+                    && comboTipo.equals(getResources().getString(R.string.str_plata))){
+                costo = 80 * 3200;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuero))
+                    && comboDije.equals(getResources().getString(R.string.str_martillo))
+                    && comboTipo.equals(getResources().getString(R.string.str_niquel))){
+                costo = 70 * 3200;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuero))
+                    && comboDije.equals(getResources().getString(R.string.str_ancla))
+                    && comboTipo.equals(getResources().getString(R.string.str_oro))){
+                costo = 120 * 3200;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuero))
+                    && comboDije.equals(getResources().getString(R.string.str_ancla))
+                    && comboTipo.equals(getResources().getString(R.string.str_rosado))){
+                costo = 120 * 3200;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuero))
+                    && comboDije.equals(getResources().getString(R.string.str_ancla))
+                    && comboTipo.equals(getResources().getString(R.string.str_plata))){
+                costo = 100 * 3200;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuero))
+                    && comboDije.equals(getResources().getString(R.string.str_ancla))
+                    && comboTipo.equals(getResources().getString(R.string.str_niquel))){
+                costo = 90 * 3200;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuerda))
+                    && comboDije.equals(getResources().getString(R.string.str_martillo))
+                    && comboTipo.equals(getResources().getString(R.string.str_oro))){
+                costo = 90 * 3200;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuerda))
+                    && comboDije.equals(getResources().getString(R.string.str_martillo))
+                    && comboTipo.equals(getResources().getString(R.string.str_rosado))){
+                costo = 120 * 3200;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuerda))
+                    && comboDije.equals(getResources().getString(R.string.str_martillo))
+                    && comboTipo.equals(getResources().getString(R.string.str_plata))){
+                costo = 70 * 3200;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuerda))
+                    && comboDije.equals(getResources().getString(R.string.str_martillo))
+                    && comboTipo.equals(getResources().getString(R.string.str_niquel))){
+                costo = 50 * 3200;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuerda))
+                    && comboDije.equals(getResources().getString(R.string.str_ancla))
+                    && comboTipo.equals(getResources().getString(R.string.str_oro))){
+                costo = 110 * 3200;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuerda))
+                    && comboDije.equals(getResources().getString(R.string.str_ancla))
+                    && comboTipo.equals(getResources().getString(R.string.str_rosado))){
+                costo = 110 * 3200;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuerda))
+                    && comboDije.equals(getResources().getString(R.string.str_ancla))
+                    && comboTipo.equals(getResources().getString(R.string.str_plata))){
+                costo = 90 * 3200;
+            }
+            if(comboMaterial.equals(getResources().getString(R.string.str_cuerda))
+                    && comboDije.equals(getResources().getString(R.string.str_ancla))
+                    && comboTipo.equals(getResources().getString(R.string.str_niquel))){
+                costo = 80 * 3200;
+            }
         }
 
     }
-
-}
